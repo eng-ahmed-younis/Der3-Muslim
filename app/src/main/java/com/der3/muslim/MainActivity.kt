8d8a29876a0c5627582265fa5189f39bea0d8caa
Ahmed Aly <ahmed.aly@gen-c.com>
2026-02-26 16:51:08 +0200

    Implement onboarding feature with navigation and UI components

--- app/src/main/java/com/der3/muslim/MainActivity.kt
+++ app/src/main/java/com/der3/muslim/MainActivity.kt
@@ -27,18 +27,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Der3MuslimTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
